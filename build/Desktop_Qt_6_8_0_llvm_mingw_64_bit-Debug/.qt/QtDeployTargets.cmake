3e9f398290e036f2210feef7e9354602440d9d6b
panjk0518 <50908468+panjk0518@users.noreply.github.com>
2024-07-19 19:29:02 +0800

    Removed unnecessary files from git

--- build/Desktop_Qt_6_8_0_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_AutoTTS_FILE C:/Users/panjk0518/Documents/Qt/AutoTTS/build/Desktop_Qt_6_8_0_llvm_mingw_64_bit-Debug/AutoTTS.exe)
-set(__QT_DEPLOY_TARGET_AutoTTS_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_AutoTTS_RUNTIME_DLLS C:/Qt/6.8.0/llvm-mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/llvm-mingw_64/bin/Qt6TextToSpeech.dll;C:/Qt/6.8.0/llvm-mingw_64/bin/Qt6Multimedia.dll;C:/Qt/6.8.0/llvm-mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/llvm-mingw_64/bin/Qt6Network.dll;C:/Qt/6.8.0/llvm-mingw_64/bin/Qt6Core.dll)
